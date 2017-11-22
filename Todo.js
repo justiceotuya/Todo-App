d0db682c5f31d1d9c4702d1ceabc430b7fce6a15
Justice Otuya <jotuya2@gmail.com>
2017-11-22 09:35:21 +0100

    First Commit

---
+++ Todo.js
@@ -0,0 +1,25 @@
+//check off specific todos by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("change");
+});
+
+//fading and deleting a todo item
+$("ul").on("click","span", function(event){
+        $(this).parent().fadeOut(500, function(){
+            $(this).remove();
+        });
+        event.stopPropagation();
+});
+
+//addin new Todo Items
+$("input").keypress(function (e) {
+    if(e.which == 13){
+        //grabbing new todo text from input and creating a new li
+        $("ul").append("<li><span>x</span> " + $(this).val() + "</li>");
+        
+        //clearing value from input box
+        $("input").val(" ");
+    }
+    
+});
+
